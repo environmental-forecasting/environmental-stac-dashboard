0636434d2ab6eb8c2ed2724f441447762b03cfb4
Bryn Noel Ubald <55503826+bnubald@users.noreply.github.com>
2024-11-19 21:56:23 +0000

    Add footer based on BAS style guide

---
+++ src/components/footer.py
@@ -0,0 +1,107 @@
+import datetime as dt
+
+import dash_bootstrap_components as dbc
+from dash import html
+
+footer_layout = html.Footer(
+    className="site-footer",
+    style={"position": "fixed", "bottom": 0, "left": 0, "width": "100%"},
+    children=[
+        html.Div(
+            className="bsk-footer bsk-footer-default m-0 pb-3",
+            children=[
+                html.Div(
+                    className="bsk-container",
+                    children=[
+                        html.Div(
+                            html.A(
+                                "Is something wrong with this page?",
+                                href="/feedback",
+                                target="_blank",
+                            ),
+                            className="bsk-footer-is-something-wrong d-inline",
+                        ),
+                        html.Div(className="bsk-footer-divider"),
+                        html.Div(
+                            className="bsk-footer-governance",
+                            style={"display": "inline-block"},
+                            children=[
+                                "The ",
+                                html.A(
+                                    "British Antarctic Survey",
+                                    href="https://www.bas.ac.uk/",
+                                ),
+                                " (BAS) is part of ",
+                                html.A(
+                                    "UK Research and Innovation",
+                                    href="https://www.ukri.org/",
+                                ),
+                                " (UKRI)",
+                                html.Div(
+                                    className="bsk-footer-ogl",
+                                    children=[
+                                        html.Div(
+                                            className="bsk-ogl-symbol",
+                                            children=[
+                                                html.A(
+                                                    href="http://www.nationalarchives.gov.uk/doc/open-government-licence",
+                                                    children=[
+                                                        html.Span(
+                                                            className="bsk-ogl-symbol",
+                                                            children=[
+                                                                "Open Government Licence"
+                                                            ],
+                                                        )
+                                                    ],
+                                                ),
+                                            ],
+                                        ),
+                                        "All content is available under the ",
+                                        html.A(
+                                            "Open Government Licence",
+                                            href="http://www.nationalarchives.gov.uk/doc/open-government-licence",
+                                        ),
+                                        ", v3.0 except where otherwise stated",
+                                    ],
+                                ),
+                            ],
+                        ),
+                        html.Div(
+                            className="bsk-footer-policy-links",
+                            style={
+                                "float": "right",
+                                "clear": "right",
+                                "display": "inline-block",
+                            },
+                            children=[
+                                html.Ul(
+                                    className="bsk-list-inline",
+                                    children=[
+                                        html.Li(
+                                            [
+                                                html.A(
+                                                    "Cookies",
+                                                    href="/cookies",
+                                                )
+                                            ],
+                                            className="d-inline-block me-2",
+                                        ),
+                                        html.Li(
+                                            [html.A("Copyright", href="/copyright")],
+                                            className="d-inline-block me-2",
+                                        ),
+                                        html.Li(
+                                            [html.A("Privacy", href="/privacy")],
+                                            className="d-inline-block",
+                                        ),
+                                    ],
+                                ),
+                                f"{dt.date.today().year} British Antarctic Survey",
+                            ],
+                        ),
+                    ],
+                ),
+            ],
+        )
+    ],
+)
