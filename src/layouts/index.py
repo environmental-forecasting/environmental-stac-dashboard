882dd124c04c5dbaf8ac4bdf63eb7fef2d0d380e
Bryn Noel Ubald <55503826+bnubald@users.noreply.github.com>
2024-11-20 03:45:47 +0000

    Start using Mantine components, include wraparound with MantineProvider

--- src/layouts/index.py
+++ src/layouts/index.py
@@ -1,38 +1,43 @@
 from components import footer, header, sidebar, map
-from dash import html
+from dash import html, _dash_renderer
 import dash_bootstrap_components as dbc
+import dash_mantine_components as dmc
 
-layout = dbc.Container(
-    className="g-0",
-    fluid=True,
-    style={
-        "height": "100vh",  # Ensure the row takes up full viewport height
-        "display": "flex",  # Use flexbox for the row layout
-        "flexDirection": "column",  # Stack children vertically
-    },
-    children=[
-        dbc.Row(dbc.Col(header.header_layout, width=12)),
-        dbc.Row(
-            [
-                dbc.Col(sidebar.sidebar_layout, width=2),
-                dbc.Col(
-                    html.Div(
-                        map.leaflet_map,
-                        style={
-                            "height": "100%",  # Let the map fill the parent container
-                            "flex": "1",  # Make sure the map takes available space in the column
-                        },
+_dash_renderer._set_react_version("18.2.0")
+
+layout = dmc.MantineProvider(
+    dbc.Container(
+        className="g-0",
+        fluid=True,
+        style={
+            "height": "100vh",  # Ensure the row takes up full viewport height
+            "display": "flex",  # Use flexbox for the row layout
+            "flexDirection": "column",  # Stack children vertically
+        },
+        children=[
+            dbc.Row(dbc.Col(header.header_layout, width=12)),
+            dbc.Row(
+                [
+                    dbc.Col(sidebar.sidebar_layout, width=2),
+                    dbc.Col(
+                        html.Div(
+                            map.leaflet_map,
+                            style={
+                                "height": "100%",  # Let the map fill the parent container
+                                "flex": "1",  # Make sure the map takes available space in the column
+                            },
+                        ),
+                        width=10,
                     ),
-                    width=10,
-                ),
-            ],
-            className="g-0",
-            style={
-                "height": "100vh",  # Ensure the row takes up full viewport height
-                "display": "flex",  # Use flexbox for the row layout
-                "flexDirection": "row",  # Stack children horizontally
-            },
-        ),
-        dbc.Row(dbc.Col(footer.footer_layout, width=12)),
-    ],
+                ],
+                className="g-0",
+                style={
+                    "height": "100vh",  # Ensure the row takes up full viewport height
+                    "display": "flex",  # Use flexbox for the row layout
+                    "flexDirection": "row",  # Stack children horizontally
+                },
+            ),
+            dbc.Row(dbc.Col(footer.footer_layout, width=12)),
+        ],
+    )
 )
