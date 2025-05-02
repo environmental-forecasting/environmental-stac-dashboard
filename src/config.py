38781e4592474e3e9584e78bbfd698c1541d3f0d
Bryn Noel Ubald <55503826+bnubald@users.noreply.github.com>
2025-05-02 21:22:27 +0100

    refactor: Update CATALOG_PATH to URL path than local file path

--- src/config.py
+++ src/config.py
@@ -1,4 +1,4 @@
 TITILER_URL = "http://localhost:8000"
 DATA_URL = "http://localhost:8002"
-CATALOG_PATH = "data/stac/catalog.json"
-# CATALOG_PATH = "http://localhost:8002/data/stac/catalog.json"
+# CATALOG_PATH = "data/stac/catalog.json"
+CATALOG_PATH = f"{DATA_URL}/data/stac/catalog.json"
