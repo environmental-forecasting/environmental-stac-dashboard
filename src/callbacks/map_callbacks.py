c67210a0b10b41bbb0abe83cf8d1824a47c131f5
Bryn Noel Ubald <55503826+bnubald@users.noreply.github.com>
2025-05-02 21:26:12 +0100

    chore: Tidy-up redundant custom TileJSON approach

--- src/callbacks/map_callbacks.py
+++ src/callbacks/map_callbacks.py
@@ -1,12 +1,9 @@
 import logging
 
 import dash_leaflet as dl
-import requests
 from config import CATALOG_PATH, DATA_URL, TITILER_URL
 from dash import ALL, MATCH, Input, Output, State, no_update
 from stac.process import get_cog_path, get_collections, get_leadtime
-
-COG_FILENAME = "sample.tif"
 
 import os
 from urllib.parse import urlparse, urlunparse
@@ -37,19 +34,6 @@ def get_tile_url(cog_path):
     cog_path = normalise_url_path(f"{DATA_URL}/{cog_path}")
     return f"{TITILER_URL}/cog/tiles/WebMercatorQuad/{{z}}/{{x}}/{{y}}?url={cog_path}"
 
-# Function to generate tile URL for a STAC Item
-# def get_tile_url(stac_item):
-#     print("stac_item:", stac_item)
-#     stac_item_url = stac_item.get_self_href()
-#     print("stac_item_url:", stac_item_url)
-#     # return f"{TITILER_URL}/stac/tiles/WebMercatorQuad/{{z}}/{{x}}/{{y}}?url={stac_item_url}&assets=cog"
-#     # stac_item_url = "http://localhost:8002/data/stac/north/forecast-2024-11-12/leadtime-0/leadtime-0.json"
-#     return f"{TITILER_URL}/stac/tiles/WebMercatorQuad/{{z}}/{{x}}/{{y}}?url={stac_item_url}&assets=geotiff"
-
-# # Load the STAC catalog
-# import pystac
-# catalog = pystac.Catalog.from_file("http://localhost:8002/data/stac/catalog.json")
-# items = list(catalog.get_all_items())
 
 # Callback function that will update the output container based on input
 def register_callbacks(app):
@@ -93,28 +77,6 @@ def register_callbacks(app):
 
         return tile_layers
 
-
-
-        # # Standard COG endpoint
-        # cog_tile_url = f"{TITILER_URL}/cog/tiles/WebMercatorQuad/{{z}}/{{x}}/{{y}}?url={COG_FILENAME}&colormap_name={colormap}&rescale=0,1"
-
-        # STAC + TileJSON endpoint
-        # request_url = f"{TITILER_URL}/tiles/{hemisphere}/{forecast_date}/{leadtime}/tilejson.json"
-        # tilejson = requests.get(request_url).json()
-        # cog_tile_url = tilejson["tiles"][0] + f"&colormap_name={colormap}&rescale=0,1"
-
-        # request_url = f"{TITILER_URL}/stac/forecast-{forecast_date}/tilejson.json"
-        # tilejson = requests.get(request_url).json()
-        # print(tilejson)
-        # cog_tile_url = tilejson["tiles"][0] + f"&colormap_name={colormap}&rescale=0,1"
-        # print(colormap, cog_tile_url)
-
-        # return cog_tile_url
-
-        # # request_url = f"{TITILER_URL}/stac/forecast-{forecast_date}/tilejson.json"
-        # request_url = f"{TITILER_URL}/stac/WebMercatorQuad/tilejson.json"
-        # # request_url = f"{TITILER_URL}/stac/tiles/forecast-{forecast_date}/tilejson.json"
-        # return request_url
 
     @app.callback(Output({'type': 'cog-collections', 'index': ALL}, 'opacity'), Input("opacity-slider", "value"), State({'type': 'cog-collections', 'index': ALL}, 'opacity'))
     def update_cog_layer_opacity(opacity, current_opacity):
