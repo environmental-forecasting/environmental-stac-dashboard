1cc355a8cee2a288d87bc36d392992a5cac9d0b1
Bryn Noel Ubald <55503826+bnubald@users.noreply.github.com>
2025-05-18 19:03:08 +0100

    refactor: Update config vars to pick-up from env

--- src/config.py
+++ src/config.py
@@ -1,4 +1,10 @@
-TITILER_URL = "http://localhost:8000"
-DATA_URL = "http://localhost:8002"
-# CATALOG_PATH = "data/stac/catalog.json"
-CATALOG_PATH = f"{DATA_URL}/data/stac/catalog.json"
+import os
+
+# Get config from environmental variables
+TITILER_URL = os.getenv("TITILER_URL", "http://localhost:8000")
+DATA_URL = os.getenv("DATA_URL", "http://localhost:8002")
+CATALOG_PATH = os.getenv("CATALOG_PATH", f"{DATA_URL}/data/stac/catalog.json")
+
+print("TITILER URL:", TITILER_URL)
+print("DATA URL:", DATA_URL)
+print("CATALOG_PATH:", CATALOG_PATH)
