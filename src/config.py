5195e00d46aae7bf3af62528490939d8d5d9764a
Bryn Noel Ubald <55503826+bnubald@users.noreply.github.com>
2025-05-19 00:03:06 +0100

    chore: Switch print for logging

--- src/config.py
+++ src/config.py
@@ -1,10 +1,11 @@
 import os
+import logging
 
 # Get config from environmental variables
 TITILER_URL = os.getenv("TITILER_URL", "http://localhost:8000")
 DATA_URL = os.getenv("DATA_URL", "http://localhost:8002")
 CATALOG_PATH = os.getenv("CATALOG_PATH", f"{DATA_URL}/data/stac/catalog.json")
 
-print("TITILER URL:", TITILER_URL)
-print("DATA URL:", DATA_URL)
-print("CATALOG_PATH:", CATALOG_PATH)
+logging.info("TITILER URL:", TITILER_URL)
+logging.info("DATA URL:", DATA_URL)
+logging.info("CATALOG_PATH:", CATALOG_PATH)
